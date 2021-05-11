836a0e0ae4fc26d62d43d574512b56cf55e7f159
Joanna Lisiak <joanna.lisiak13@gmail.com>
2021-05-11 20:43:41 +0200

    Create a new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,10 +7,24 @@ class ArticlesController < ApplicationController
 
   def show
   end
+  
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
 
   private
 
   def set_article
     @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
